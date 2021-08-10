10eae5a3aba973df09f19159211f15ab4a968961
Gilbertkitetu <gilbertkitetu2@gmail.com>
2021-08-10 12:04:02 +0300

    #####

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -17,20 +17,27 @@ function Login(){
         <div className="login">
             <div>
                 <div>
-                <label>Email: </label>
-                <input type="text" placeholder="Enter your email" className='input-control'
+                <label>Username or email address* </label>
+                <input type="text"  className='input-control'
                 value={user_email} onChange={(e) => {set_user_email(e.target.value)}}
                 />
                 </div>
 
                 <div>
-                <label>Password: </label>
-                <input type="password" placeholder="Enter your password" className='input-control'
+                <label>Password* </label>
+                <input type="password"  className='input-control'
                 value={password} onChange={(e) => {set_password(e.target.value)}}
                 />
                 </div>
+                <div className="checkbox">
+                <input type="checkbox"/><label>Remember me</label>
+                <p>Forgot password?</p>
+                </div>
 
                 <button className="btn-login">Login</button>
+
+                <div><p>Or</p></div>
+                <button className="btn-login">Create an Account</button>
             </div>
         </div>
         </div>
