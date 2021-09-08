7ae5a104533de3885cbfe7204083dc945bc9b451
Gilbert kitetu <gilbertkitetu2@gmail.com>
2021-09-08 07:35:06 +0300

    jwt

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,62 @@
 const express = require('express');
 const router = express.Router();
+var jwt = require('jsonwebtoken')
 
 const mongoose = require('mongoose');
 
 const userModel = require('../models/user_model');
 
+// Verify token
+function  verifyToken(req, res, next) {
+    //Get auth header value 
+    const bearerHeader = req.headers['authorization'];
+    //check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined') {
+        //Split at the space
+        const bearer = bearerHeader.split(' ');
+        //Get token from array
+        const bearerToken = bearer[1];
+        //set the token
+        req.token = bearerToken;
+        //Next middleware
+        next();
+    } else {
+        //Forbidden
+        res.sendStatus(403)
+    }
+}
 
+router.post('/login', (req, res) => {
+
+    //mock user
+    const user = {
+        id: 1,
+        username: 'Gilbert',
+        email: 'gilbertkitetu1@gmail.com'
+    }
+
+    jwt.sign({ user: user }, 'mysecretkey', { expiresIn: '1h'},(err, token) => {
+        res.json({
+            token: token
+        });
+    });
+
+    //res.send("This is the login route")
+})
+
+app.post('/create-shop', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'mysecretkey', (err, authData) => {
+        if(err) {
+            res.sendStatus(403);
+        }else {
+            res.json({
+                message: 'Shop Created succesfully',
+                authData
+            });
+        }
+    });
+  
+});
 
 router.get('/get_users', (req, res) => {
     res.send("This is a get users route");
